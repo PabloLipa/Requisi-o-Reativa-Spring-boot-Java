fca11ce7f644d5d71259d88217688b70221f37bd
pablo <pablohenriquessl326@outlook.com>
2024-10-13 13:55:21 -0300

    Java Reativo

---
+++ src/main/java/com/example/springproject/reqsitions/ReqsitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springproject.reqsitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqsitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReqsitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springproject/reqsitions/ReqsitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproject.reqsitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReqsitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
